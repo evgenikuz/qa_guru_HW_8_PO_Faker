450fa577ee66dde66290be999b01c00e2fa61c67
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-07-04 12:43:47 +0300

    deleted main, changed getCityForState, phoneNumber and birthday method

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -10,37 +10,27 @@ public class TestData {
             firstName = faker.name().firstName(),
             lastName = faker.name().lastName(),
             email = faker.internet().emailAddress(),
-            phoneNumber = faker.numerify("##########"),
             gender = faker.options().option("Male", "Female", "Other"),
+            phoneNumber = faker.phoneNumber().subscriberNumber(10),
             streetAddress = faker.address().streetAddress(),
             subject = faker.options().option("Maths", "Hindi", "Biology", "Accounting", "Chemistry"),
             hobby = faker.options().option("Sports", "Reading", "Music"),
-            avatar = faker.options().option("av1.jpeg", "av2.jpg", "av3.jpg");
+            avatar = faker.options().option("av1.jpeg", "av2.jpg", "av3.jpg"),
+            day = String.format("%02d", faker.number().numberBetween(1, 28)),
+            month = faker.options().option("January", "February", "March", "April",
+                                           "May", "June", "July", "August", "September",
+                                           "October", "November", "December"),
+            year = String.format("%s", faker.number().numberBetween(1920, 2024)),
+            state = faker.options().option("NCR", "Uttar Pradesh", "Rajasthan", "Haryana"),
+            city = getCityForState(state);
 
-    static Date birthday = faker.date().birthday();
-    public static String year = String.valueOf(birthday.getYear() + 1900);
-    static int birthdayMonth = birthday.getMonth();
-    static String[] months = {"January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"};
-    public static String month = months[birthdayMonth];
-    static int bday = birthday.getDate();
-    public static String day = (bday < 10) ? "0" + bday : String.valueOf(bday);
-
-    public static String state = faker.options().option("NCR", "Uttar Pradesh", "Rajasthan", "Haryana");
-    public static String city = getCityForState(state);
     public static String getCityForState(String state) {
-        if (state.equals("NCR")) {
-            city = faker.options().option("Delhi", "Gurgaon", "Noida");
-        }
-        else if (state.equals("Uttar Pradesh")) {
-            city = faker.options().option("Agra", "Lucknow", "Merrut");
-        }
-        else if (state.equals("Rajasthan")) {
-            city = faker.options().option("Jaipur", "Jaiselmer");
-        }
-        else if (state.equals("Haryana")) {
-            city = faker.options().option("Karnal", "Panipat");
-        }
-        return city;
+        return switch (state) {
+            case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Uttar Pradesh" -> faker.options().option("Agra", "Lucknow", "Merrut");
+            case "Haryana" -> faker.options().option("Karnal", "Panipat");
+            case "Rajasthan" -> faker.options().option("Jaipur", "Jaiselmer");
+            default -> null;
+        };
     }
 }
